8072d728e09182bf39708aed5ed6c1bf14609d10
lanoix-a <Arnaud.Lanoix@univ-nantes.fr>
2023-06-12 11:27:35 +0200

    code example included

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,11 @@ import iut.info1.pickomino.Connector
 
 fun main() {
     println("Hello students !!!")
-
     val connect = Connector.factory("0.0.0.0", "8080")
-    println(connect.listOfGameIds())
+    println("Parties actives sur le serveur = ${connect.listOfGameIds()}")
+    val identification = connect.newGame(2)
+    val id = identification.first
+    val key = identification.second
+    val currentGame = connect.gameState(id, key)
+    println("Nouvelle partie = $currentGame")
 }
