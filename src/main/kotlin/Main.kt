3caf2dc43998b6caa22ad10416caf6c4efd344ee
lanoix-a <Arnaud.Lanoix@univ-nantes.fr>
2023-06-12 13:47:51 +0200

    real server configured

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,9 @@ import iut.info1.pickomino.Connector
 
 fun main() {
     println("Hello students !!!")
-    val connect = Connector.factory("0.0.0.0", "8080")
+    val connect = Connector.factory("172.26.82.76", "8080")
     println("Parties actives sur le serveur = ${connect.listOfGameIds()}")
-    val identification = connect.newGame(2)
+    val identification = connect.newGame(3)
     val id = identification.first
     val key = identification.second
     val currentGame = connect.gameState(id, key)
